1c14a43efe07ca266ddb57b661dedd054a980aea
der1x <130235333+Mark4353@users.noreply.github.com>
2025-03-27 16:39:54 +0200

    hw11

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-const minute = 60;
+const textTimer = document.querySelector(".timer1");
 
-const timer1 = updateTimer(()=>{
-    console.log(`Залишилось: ${minute} хвилин`);    
-    if (minute === 30) {
-        console.log("Залишилось менше половини часу");
-    }
-    if (minute > 0) {
-        minute--;
-        setTimeout(updateTimer, 60000); 
-    }
-}) ;
+let minute = 60;
+
+function updateTimer() {
+  textTimer.textContent = `Таймер на ${minute} хвилин`;
+
+  if (minute === 30) {
+    console.log("Залишилось менше половини часу");
+  } else if (minute > 0) {
+    minute--;
+    setTimeout(updateTimer, 60000);
+  }
+}
+
 updateTimer();
 
+
+
+const textTimer2 = document.querySelector(".timer2");
+const restartButton = document.querySelector(".restartButton");
+const message = document.querySelector(".message");
+
+let millisecondsLeft = 30000; 
+const interval = 10; 
+
+function updateTimer2(){
+  textTimer2.textContent = `Залишилось: ${(millisecondsLeft / 1000).toFixed(2)} секунд`;
+
+  if (millisecondsLeft <= 10000) {
+    textTimer2.style.color= "red"
+
+          }
+
+  if (millisecondsLeft > 0) {
+    millisecondsLeft -= interval;
+    setTimeout(updateTimer2, interval);
+    
+  } else {
+    textTimer2.textContent = "Час вийшов";
+  }
+
+  restartButton.addEventListener("click", ()=> {
+    millisecondsLeft = 29000;
+
+}) ;
+}
+
+updateTimer2();
